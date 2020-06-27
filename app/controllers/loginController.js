b83ee6155f96ae59219c9eabe7cbfa26edb1bdbe
user <mahesworrajshrestha@gmail.com>
2020-06-28 01:19:44 +0545

    [#16] only verified user can get login into the system

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -4,34 +4,46 @@ var jwt = require("jsonwebtoken");
 const SECRET_KEY = "secret_key";
 
 async function login(req, res) {
-  console.log(req.body.password + req.body.email);
   const password = req.body.password;
   try {
     const result = await user.findOne({
       where: { email: req.body.email },
     });
-
     if (result) {
-      const passwordFromDB = result.password;
-      const id = result.user_id;
-      const isMatchPassword = bcrypt.compareSync(password, passwordFromDB);
-      if (isMatchPassword) {
-        var usertoken = jwt.sign({ userEmail: req.body.email }, SECRET_KEY);
-        res.json({
-          status: true,
-          usertoken: usertoken,
-          type: result.userType,
-          message: "Login Success",
-          id: id,
-          // accessToken:
-          //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJyYW1AZ21haWwuY29tIiwiaWF0IjoxNTkzMTA3MTEwfQ.jU8-NdkZ_dXrzlYmTxPYqcQJKjkuvlnsajKi_n3dEZM",
-          // userType: "Admin",
-          // message: "Login Success",
+      try {
+        const verified = await user.findOne({
+          where: { verified: 1 },
         });
-      } else {
+        if (verified) {
+          const passwordFromDB = result.password;
+          const id = result.user_id;
+          const isMatchPassword = bcrypt.compareSync(password, passwordFromDB);
+          if (isMatchPassword) {
+            var usertoken = jwt.sign({ userEmail: req.body.email }, SECRET_KEY);
+            res.json({
+              status: true,
+              usertoken: usertoken,
+              type: result.userType,
+              message: "Login Success",
+              id: id,
+            });
+          } else {
+            res.json({
+              status: false,
+              message: "Password do not match",
+            });
+          }
+        } else {
+          res.json({
+            status: false,
+            message: "you are not verified user ifelse",
+          });
+        }
+      } catch (error) {
+        console.log(error);
         res.json({
           status: false,
-          message: "Password do not match",
+          message: "you are not verified user",
         });
       }
     } else {
